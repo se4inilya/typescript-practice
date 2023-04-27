4ea73aa72602e914e20209365addd4a317c44aea
Sechin Ilya <se4in.ilya1337@gmail.com>
2023-04-27 14:13:22 +0300

    simple app made

---
+++ rrts/src/props/Child.tsx
@@ -0,0 +1,13 @@
+interface ChildProps {
+    color: string,
+    onClick: () => void,
+}
+
+export const Child: React.FC<ChildProps> = ({color, onClick}) => {
+    return (
+        <div>
+            {color}
+            <button onClick={onClick}>Click me</button>
+        </div>
+    );
+}
---
+++ rrts/src/props/Parent.tsx
@@ -0,0 +1,5 @@
+import { Child } from "./Child";
+
+export const Parent = () => {
+    return <Child color="red" onClick={()=>console.log("Clicked")} />;
+}
